a3e9818793b1bc11d3922b356d0cbb6ab60b6726
Pavel Pilatov <pilatov@gmail.com>
2022-04-13 17:16:31 +0300

    prepare for HomeWork #4

--- src/test/java/guru/qa/AutomationPracticeFormTests.java
+++ src/test/java/guru/qa/AutomationPracticeFormTests.java
@@ -21,22 +21,22 @@ public class AutomationPracticeFormTests {
     @Test
     void fillFormTest() {
 
-        String name = "Pavel";
-        String fname = "Pilatov";
-        String email = fname.toLowerCase() + "@gmail.com";
+        String firstName = "Pavel";
+        String lastName = "Pilatov";
+        String email = lastName.toLowerCase() + "@gmail.com";
         String mobile = "9265001234";
         String address = "Russia, Khimki";
 
         open("/automation-practice-form");
-        $("#firstName").setValue(name);
-        $("#lastName").setValue(fname);
+        $("#firstName").setValue(firstName);
+        $("#lastName").setValue(lastName);
         $("#userEmail").setValue(email);
         $("#genterWrapper").$(byText("Male")).click();
         $("#userNumber").setValue(mobile);
         $("#dateOfBirthInput").click();
         $(".react-datepicker__month-select").selectOption("May");
         $(".react-datepicker__year-select").selectOption("1982");
-        $(".react-datepicker__day--002:not(react-datepicker__day--outside-month)").click();
+        $(".react-datepicker__day--002:not(.react-datepicker__day--outside-month)").click();
         $("#subjectsInput").setValue("Co").pressEnter();
         $("#hobbiesWrapper").$(byText("Sports")).click();
         $("#uploadPicture").uploadFromClasspath("mypic1.jpg");
@@ -47,7 +47,7 @@ public class AutomationPracticeFormTests {
         $("#stateCity-wrapper").$(byText("Delhi")).click();
         $("#submit").click();
         $("#example-modal-sizes-title-lg").shouldHave(text("Thanks for submitting the form"));
-        $(".table-responsive").shouldHave(text("Student Name " + name + " " + fname),
+        $(".table-responsive").shouldHave(text("Student Name " + firstName + " " + lastName),
                 text("Mobile " + mobile),
                 text("Picture mypic1.jpg"),
                 text("Student Email " + email),
